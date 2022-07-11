18750ea88a095149615f50e70ce3c0d53e9ab774
Guhan <you@example.com>
2022-07-11 18:55:21 +0530

    changes

--- src/pages/ViewModel.js
+++ src/pages/ViewModel.js
@@ -1,64 +1,79 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react'
-import { useNavigate, useParams } from 'react-router-dom';
-import Navbar from './Navbar'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import Navbar from "./Navbar";
 
 function ViewModel() {
-    function fetch() {
-        if (!localStorage.getItem("myapptoken")) {
-            navigate("/");
-        }
+  function fetch() {
+    if (!localStorage.getItem("myapptoken")) {
+      navigate("/");
     }
-    const params= useParams()
-    const navigate = useNavigate()
-    const [user, setUserData] = useState([]);
-    useEffect(() => {
-        async function fetchData() {
-          let user = await axios.get(`http://localhost:8080/getProduct/${params.id}`,
-          {
-            headers: {
-              Authorization: window.localStorage.getItem('myapptoken'),
-            },
-          });
-          setUserData(user.data);
+  }
+  const params = useParams();
+  const navigate = useNavigate();
+  const [user, setUserData] = useState([]);
+  useEffect(() => {
+    async function fetchData() {
+      let user = await axios.get(
+        `http://localhost:8080/getProduct/${params.id}`,
+        {
+          headers: {
+            Authorization: window.localStorage.getItem("myapptoken"),
+          },
         }
-        fetchData();
-      }, []);
-      useEffect(() => {
-        fetch()
-    },[])
+      );
+      setUserData(user.data);
+      console.log(user.data);
+    }
+    fetchData();
+  }, []);
+  useEffect(() => {
+    fetch();
+  }, []);
+
   return (
     <>
-        <Navbar/>
-        <div class="card w-50">
-            <div class="card-body">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-6">
-                            <h3 class="card-title">Model Details</h3>
-                            <p class="card-text">Name:<strong>{user.title}</strong></p>
-                            <p class="card-text">Mobile:<strong>{user.model}</strong></p>
-                            <p class="card-text">Price:<strong>{user.price}</strong></p>
-                            <p class="card-text">OfferPrice:<strong>{user.offerPrice}</strong></p>
-                            <div className="col-lg-6 mt-3">
-                                <input
-                                    type={"submit"}
-                                    value="Close"
-                                    onClick={() => navigate("/view/mobile", { replace: true })}
-                                    className="btn btn-primary"
-                                />
-                            </div>
-                        </div>
-                        <div className="col-6">
-                            <img src={user.img} className="img-fluid"/>
-                        </div>
-                    </div>
+      <Navbar />
+      <div class="card w-50">
+        <div class="card-body">
+          <div className="container">
+            <div className="row">
+              <div className="col-6">
+                <h3 class="card-title">Model Details</h3>
+                <p class="card-text">
+                  Name:<strong>{user.title}</strong>
+                </p>
+                <p class="card-text">
+                  Mobile:<strong>{user.model}</strong>
+                </p>
+                <p class="card-text">
+                  Price:<strong>{user.price}</strong>
+                </p>
+                <p class="card-text">
+                  OfferPrice:<strong>{user.offerPrice}</strong>
+                </p>
+                <div className="col-lg-6 mt-3">
+                  <input
+                    type={"submit"}
+                    value="Close"
+                    onClick={() => navigate("/view/mobile", { replace: true })}
+                    className="btn btn-primary"
+                  />
                 </div>
-
+              </div>
+              <div className="col-6">
+                {console.log(user)}
+                <img
+                  src={`http://localhost:8080/${user.image}`}
+                  className="img-fluid preview-img"
+                />
+              </div>
             </div>
+          </div>
         </div>
-        </>
-  )
+      </div>
+    </>
+  );
 }
 
-export default ViewModel
+export default ViewModel;
