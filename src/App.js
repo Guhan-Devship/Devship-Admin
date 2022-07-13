29ee03d506ddcd755fa0c46619e86d12c80a57d6
Guhan <you@example.com>
2022-07-13 16:47:36 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,36 +1,38 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import "bootstrap/dist/css/bootstrap.min.css"
-import 'react-toastify/dist/ReactToastify.css';
-import './App.css';
-import Login from './pages/Login';
-import Home from './pages/Home';
-import Users from './pages/Users';
-import CreateUser from './pages/CreateUser';
-import EditUser from './pages/EditUser';
-import ViewUser from './pages/ViewUser';
-import Products from './pages/Products';
-import CreateProduct from './pages/CreateProduct';
-import ViewProduct from './pages/ViewProduct';
-import CreateMobile from './pages/CreateMobile';
-import ViewModel from './pages/ViewModel';
-import EditMobile from './pages/EditMobile';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "react-toastify/dist/ReactToastify.css";
+import "./App.css";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import Users from "./pages/Users";
+import CreateUser from "./pages/CreateUser";
+import EditUser from "./pages/EditUser";
+import ViewUser from "./pages/ViewUser";
+import Products from "./pages/Products";
+import CreateProduct from "./pages/CreateProduct";
+import ViewProduct from "./pages/ViewProduct";
+import CreateMobile from "./pages/CreateMobile";
+import ViewModel from "./pages/ViewModel";
+import EditMobile from "./pages/EditMobile";
+import Order from "./pages/Order/Order";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login/>} />
-        <Route path="/home" element={<Home/>} />
-        <Route path="/users" element={<Users/>} />
-        <Route path="/create-user" element={<CreateUser/>} />
-        <Route path="/edit-user/:id" element={<EditUser/>} />
-        <Route path="/view-user/:id" element={<ViewUser/>} />
-        <Route path="/product" element={<Products/>} />
-        <Route path="/createProduct" element={<CreateProduct/>} />
-        <Route path="/view/:id" element={<ViewProduct/>} />
-        <Route path="/createMobile" element={<CreateMobile/>} />
-        <Route path="/viewModel/:id" element={<ViewModel/>} />
-        <Route path="/editModel/:id" element={<EditMobile/>} />
+        <Route path="/" element={<Login />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/users" element={<Users />} />
+        <Route path="/create-user" element={<CreateUser />} />
+        <Route path="/edit-user/:id" element={<EditUser />} />
+        <Route path="/view-user/:id" element={<ViewUser />} />
+        <Route path="/product" element={<Products />} />
+        <Route path="/order" element={<Order />} />
+        <Route path="/createProduct" element={<CreateProduct />} />
+        <Route path="/view/:id" element={<ViewProduct />} />
+        <Route path="/createMobile" element={<CreateMobile />} />
+        <Route path="/viewModel/:id" element={<ViewModel />} />
+        <Route path="/editModel/:id" element={<EditMobile />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/Order/Order.js
@@ -0,0 +1,76 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import Navbar from "../Navbar";
+
+function Order() {
+  const [order, setOrder] = useState([]);
+  async function fetchAll() {
+    try {
+      let listData = await axios.get("http://localhost:8080/getOrders", {
+        headers: {
+          Authorization: window.localStorage.getItem("myapptoken"),
+        },
+      });
+      setOrder(listData.data);
+      console.log(listData);
+    } catch (error) {
+      alert("Something went wrong");
+    }
+  }
+  useEffect(() => {
+    fetchAll();
+  }, []);
+  return (
+    <>
+      <Navbar />
+      <div class="d-sm-flex align-items-center justify-content-between mb-4">
+        <h1 class="h3 mb-0 text-gray-800">Orders</h1>
+      </div>
+
+      <div class="card shadow mb-4">
+        <div class="card-header py-3 d-flex justify-content-between">
+          <h6 class="m-0 font-weight-bold text-primary">Order Details</h6>
+        </div>
+        <div class="card-body">
+          <div class="table-responsive table-scroll">
+            <table
+              class="table table-bordered"
+              id="dataTable"
+              width="100%"
+              cellspacing="0"
+            >
+              <thead>
+                <tr>
+                  <th>Title</th>
+                  <th>Price</th>
+                  <th>Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                {order.map((data) => {
+                  return (
+                    <tr>
+                      <td>{data.title}</td>
+                      <td>{data.offerPrice}</td>
+                      <td>
+                        <button className="btn btn-outline-primary btn-sm ms-2">
+                          View
+                        </button>
+                      </td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          </div>
+        </div>
+        <Link to={"/home"}>
+          <button className="btn btn-primary btn-sm m-4">Back</button>
+        </Link>
+      </div>
+    </>
+  );
+}
+
+export default Order;
